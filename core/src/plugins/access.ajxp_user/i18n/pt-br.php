1d1ed8e5efeb71a8bf8b185d5d517813b6f33b96
Ramalho Júnior <fielcomunista@users.noreply.github.com>
2016-12-07 10:36:16 -0200

    pt-br.php
    Translate to Brazilian Portuguese.

---
+++ core/src/plugins/access.ajxp_user/i18n/pt-br.php
@@ -0,0 +1,120 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"title"=> "Painel",
+"desc" => "Preferências da minha conta, meu livro de endereços, arquivos compartilhados, etc.",
+"1" => "Endereços",
+"2" => "Pastas Compartilhadas",
+"3" => "Arquivos Compartilhados",
+"4"	=> "Caminho do Arquivo",
+"5"	=> "Repositório",
+"6"	=> "Senha",
+"7"	=> "Validade",
+"8" => "Elementos",
+"9" => "Usuários",
+"10"=> "Áreas de Trabalho",
+"11"=> "Tem a certeza de que quer apagar o(s) seguintes elemento(s)?",
+"12"=> "Você não tem permissão para apagar este elemento.",
+"13"=> "Apagado com sucesso o arquivo público.",
+"14"=> "Integridade",
+"15"=> "OK",
+"16"=> "Quebrado",
+"17"=> "URL de Download",
+"18"=> "Copiar URL",
+"19"=> "Copiar URL para enviar por e-mail.",
+"20"=> "Download",
+"21"=> "Sim",
+"22"=> "Não",
+"23"=> "Apagados com sucesso %s arquivos.",
+"24"=> "Nada para apagar.",
+"25"=> "Limpar Expirados",
+"26"=> "Apagar Arquivos Compartilhados Expirados.",
+"27"=> "Proprietário",
+"28"=> "Arquivos compartilhados para transferência direta, protegidos ou não por uma senha.",
+"29"=> "Delega Áreas de Trabalho criados por Usuários.",
+"30"=> "Usuários criados para acessar às Áreas de Trabalho Delegadas.",
+"31"=> "Descrição",
+"32"=> "Zerar",
+"33"=> "Zerar Contador de Downloads",
+"34" => "Meus Compartilhamentos",
+"35" => "Todos meus itens compartilhados (arquivos e pastas)",
+"36" => "Parâmetros",
+"37" => "Meu perfil e configuração de aplicações",
+"38"=> "Notificações",
+"39"=> "Selecione uma Área de Trabalho",
+"40"=> "Bem-vindo, %s",
+"41"=> "Use esta Área de Trabalho como Padrão",
+"42"=> "Entre",
+"43"=> "Meu Perfil",
+"43t"=> "Todos os meus dados pessoais",
+"44"=> "Minha Conta",
+"45"=> "Dados Pessoais",
+"46"=> "Livro de Endereços",
+"47"=> "Usuários e Equipes que você criou",
+"48"=> "Meus Usuários",
+"49"=> "Úsuários Externos que você criou",
+"50"=> "Minhas Equipes",
+"51"=> "Equipes de usuários, usado como atalho quando compartilhando",
+"52"=> "Você tem certeza que quer apagar esta equipe? Esta ação não removerá os usuários.",
+    "53"=> "Começando",
+    "54"=> "Iniciando no Pydio (servidor FTP da Prefeitura)? Assista aos tutoriais em vídeo",
+    "55"=> "Você precisa de ajuda <a>para iniciar?</a>",
+    "56"=> "Começando no Pydio (servidor FTP da Prefeitura)? Aqui estão alguns vídeos para você!",
+    "57"=> "Download Pydio para...",
+    "58"=> "Dispositivos Android",
+    "59"=> "iPhone/iPad",
+    "60"=>"Mac OS (Beta)",
+    "61"=>"Windows (Beta)",
+    "62"=>"<h2>Começando</h2>
+            Este vídeo é uma introdução ao servidor FTP Pydio:
+            <ul>
+                <li>Painel do Usuário</li>
+                <li>Visão sobre a interface gráfica</li>
+                <li>Gerenciando arquivos e diretórios</li>
+                <li>Visualizando e Editando arquivos</li>
+                <li>Acesso rápido através de marcações e ferramenta de busca</li>
+                <li>Seleção de contêiners</li>
+            </ul>",
+    "63"=>"<h2>Compartilhando arquivos com Pydio</h2>
+                            Neste tutorial nós vamos cobrir o básico sobre compartilhamento de arquivos utilizando o Pydio:
+                            <ul>
+                                <li>Criando Web links</li>
+                                <li>Enviando para usuários internos e externos</li>
+                                <li>Seja notificado quando um link for baixado</li>
+                                <li>Criando um link protegido por senha</li>
+                                <li>Criando vários links para um arquivo</li>
+                                <li>Parando totalmente o compartilhamento de um arquivo</li>
+            </ul>",
+    "64"=>"<h2>Compartilhando pastas com Pydio</h2>
+                            Este vídeo cobre as várias opções para compartilhar pastas no Pydio
+                            <ul>
+                                <li>Compartilhar como nova Área de Trabalho</li>
+                                <li>Compartilhar como um minisite público</li>
+            </ul>",
+    "65"=> "Descobrindo mais no canal do Pydio!",
+    "66"=>"<h2>Usando o Pydio em dispositivo iOS</h2>
+                            Este vídelo explica como usar o aplicativo Pydio (disponível na Apple Store). Processo é muito semelhante ao Android.
+<ul><li>Configurando a conexão do servidor</li>
+<li>Navegando pelos arquivos</li>
+<li>Mantengo arquivos offline e interagindo com aplicações externas</li>
+<li>Protegendo o aplicativo com um código PIN</li>
+<li>Usando marcações e Ferramentas de Busca</li></ul>",
+);
